16ac342a890e001eafa512041c6d3131aa4867ac
Huseein Omar <husseinomar6190@gmail.com>
2021-12-02 12:04:54 +0300

    login form changes

--- app.js
+++ app.js
@@ -19,3 +19,27 @@ window.addEventListener('click', function(event){
         modal.classList.remove('showModal');
     }
 });
+
+function myFunction() {
+    var person = prompt("Please enter your mobile number", "9xxxxxxxxx");
+   var r =confirm("The price is $1200.00\n Press ok to continue");
+    if (person != null&& r== true) {
+        alert("Dear customer, Thank You for booking the family room!!!");
+    }
+}
+
+function doubleBed() {
+    var person = prompt("Please enter your mobile number", "9xxxxxxxxx");
+   var r =confirm("The price is $700.00\n Press ok to continue");
+    if (person != null&& r== true) {
+        alert("Dear customer, Thank You for booking the Double bedroom!!!");
+    }
+}
+
+function singleBed() {
+    var person = prompt("Please enter your mobile number", "9xxxxxxxxx");
+   var r =confirm("The price is $500.00\n Press ok to continue");
+    if (person != null&& r== true) {
+        alert("Dear customer, Thank You for booking the single bedroom!!!");
+    }
+}
